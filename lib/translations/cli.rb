6078169b2e38cf171e58371e641c0f0bd1d34f37
Marten Lienen <marten.lienen@gmail.com>
2013-02-08 02:55:51 +0100

    Add simple command parsing

--- lib/translations/cli.rb
+++ lib/translations/cli.rb
@@ -1,11 +1,40 @@
 require "translations/commands/add_command"
 
-class Translations::CLI
-  def initialize argv
-    
-  end
+module Translations
+  class CLI
+    def initialize argv
+      @command = build_command argv
+    end
 
-  def run
+    def run
+      if @command
+        @command.run
+      end
+    end
 
+    def build_command argv
+      if argv.length == 0
+        display_help_message
+
+        nil
+      else
+        command = argv.shift
+        command_class = "#{command.capitalize}Command".to_sym
+
+        if Translations::Commands.const_defined? command_class
+          Translations::Commands.const_get(command_class).new argv
+        else
+          display_help_message
+
+          nil
+        end
+      end
+    end
+
+    def display_help_message
+      puts <<-HELP
+USAGE: translations add KEY
+      HELP
+    end
   end
 end
---
+++ lib/translations/commands/add_command.rb
@@ -0,0 +1,13 @@
+module Translations
+  module Commands
+    class AddCommand
+      def initialize argv
+
+      end
+
+      def run
+
+      end
+    end
+  end
+end
